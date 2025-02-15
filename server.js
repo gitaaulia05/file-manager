622f7d26bbb9102daa1981be041f443f71935161
gitaaulia05 <gitaauliahafid@gmail.com>
2025-02-16 00:02:57 +0700

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+
+
 const { addMetadata, getAllFiles, deleteFileMetadata } = require('./fileStore');
 
 const uploadDir = path.join(__dirname, 'uploads');
 
+//Menyimpan token yang valid ( BERISI TOKEN DAN MASA KADALUARSA)
+const validTokens = new Map();
 // Pastikan folder "uploads" ada
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir);
@@ -22,7 +26,34 @@ const server = http.createServer((req, res) => {
             res.setHeader('Content-Type', 'text/html');
             res.end(data);
         });
-    } else if (req.method === 'GET' && req.url === '/style.css') {
+     } else if(req.method === 'POST' && req.url === '/storeToken'){
+        let body = '';
+        req.on('data' , chunk => {
+            body += chunk;
+        });
+        req.on('end', () => {
+            try {
+                const { token } = JSON.parse(body);
+                if(!token) {
+                    res.statusCode = 400;
+                    res.end('Invalid token');
+                    return;
+                }
+                    // MEMASTIKAN HANYA ADA SATU TOKEN YANG VALID 
+                validTokens.clear();
+
+                // KADALUARSA TOKEN 5 MENIT
+                const expiry = Date.now() + 5 * 60 * 1000;
+                validTokens.set(token , expiry);
+                res.statusCode = 200;
+                res.end('Token stored successfully');
+            } catch(err){
+                res.statusCode = 400;
+                res.end('Invalid request format');
+            }
+        });
+     } 
+    else if (req.method === 'GET' && req.url === '/style.css') {
         const cssPath = path.join(__dirname, 'style.css');
         fs.readFile(cssPath, (err, data) => {
             if (err) {
@@ -34,6 +65,24 @@ const server = http.createServer((req, res) => {
             res.end(data);
         });
     } else if (req.method === 'POST' && req.url === '/upload') {
+        const authHeader = req.headers['authorization'];
+     
+        const token = req.headers['authorization']?.split(' ')[1];
+     
+        if(!token || !validTokens.has(token)) {
+            res.statusCode = 401;
+            res.end('Invalid or Missing Token');
+            return;
+        }
+
+        const expiry = validTokens.get(token);
+    if (Date.now() > expiry) {
+        validTokens.delete(token); // Hapus token yang sudah expired
+        res.statusCode = 401;
+        res.end('Token expired');
+        return;
+    }
+
         let body = [];
         req.on('data', chunk => body.push(chunk));
         req.on('end', () => {
@@ -60,10 +109,75 @@ const server = http.createServer((req, res) => {
             });
         });
     } else if (req.method === 'GET' && req.url === '/files') {
+        const authHeader = req.headers['authorization'];
+        console.log('authorization Header:', authHeader);
+
+        if(!authHeader) {
+            res.statusCode = 401;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({
+                success: false , message : 'Acces Denied'
+            }));
+            return;
+        }
+
+        const token = authHeader.split(' ')[1];
+        console.log('token delete ' +token);
+
+        if(!token || !validTokens.has(token)) {
+            res.statusCode = 401;
+            res.setHeader('Content-Type' , 'application/json');
+            res.end(JSON.stringify({
+                success : false,
+                message : 'Invalid Token'
+            }));
+            return;
+        
+        }
+
         const files = getAllFiles();
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(files));
+        res.end(JSON.stringify( files));
+
     } else if (req.method === 'POST' && req.url.startsWith('/delete/')) {
+        const authHeader = req.headers['authorization'];
+        console.log('authorization Header:', authHeader);
+
+        if(!authHeader) {
+            res.statusCode = 401;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({
+                success: false , message : 'Missing Auth Header'
+            }));
+            return;
+        }
+
+        const token = authHeader.split(' ')[1];
+        console.log('token delete ' +token);
+
+        if(!token || !validTokens.has(token)) {
+            res.statusCode = 401;
+            res.setHeader('Content-Type' , 'application/json');
+            res.end(JSON.stringify({
+                success : false,
+                message : 'Invalid Token'
+            }));
+            return;
+        
+        }
+
+        const expiry = validTokens.get(token);
+    if (Date.now() > expiry) {
+        validTokens.delete(token); // Hapus token yang sudah expired
+        res.statusCode = 401;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({
+            success : false,
+            message :'Token expired'
+        }));
+        return;
+    }
+
         const fileName = req.url.split('/').pop();
         const filePath = path.join(uploadDir, fileName);
 
@@ -71,10 +185,18 @@ const server = http.createServer((req, res) => {
             fs.unlinkSync(filePath);
             deleteFileMetadata(fileName);
             res.statusCode = 200;
-            res.end('File deleted successfully');
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({
+                success : true,
+                message : 'File deleted successfully'
+            }));
         } else {
             res.statusCode = 404;
-            res.end('File not found');
+            res.setHeader('Content-Type', 'application/json');
+            res.end( JSON.stringify({
+                success : false,
+                message : 'File not found',
+            }));
         }
     } else {
         res.statusCode = 404;
